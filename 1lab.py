e8567219083ec81952a37c7b88adfb96efac1476
Сіплий <maksdrap228@gmail.com>
2023-06-22 22:27:30 +0300

    Переробив з використанням бібліотеки "pycryptodome".

--- 1lab.py
+++ 1lab.py
@@ -1,57 +1,55 @@
-class ECPoint:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+from Crypto.Util.number import getRandomInteger
+from Crypto.PublicKey import ECC
+
 
 def base_point_get():
     # Отримати базову точку G
-    # Реалізуйте відповідний код
-    return ECPoint(x, y)
+    curve = ECC.generate(curve='P-256')
+    return curve.pointQ
 
 def ec_point_gen(x, y):
     # Створити точку ЕК з координатами (x, y)
-    # Реалізуйте відповідний код
-    return ECPoint(x, y)
+    curve = ECC.generate(curve='P-256')
+    return curve.point(x, y)
 
 def is_on_curve_check(point):
     # Перевірити, чи належить точка point до кривої
-    # Реалізуйте відповідний код
-    return is_on_curve
+    curve = ECC.generate(curve='P-256')
+    return curve.contains(point)
 
 def add_ec_points(a, b):
     # Додати точки a і b на ЕК
-    # Реалізуйте відповідний код
-    return c
+    curve = ECC.generate(curve='P-256')
+    return a + b
 
 def double_ec_point(a):
     # Подвоїти точку a на ЕК
-    # Реалізуйте відповідний код
-    return c
+    curve = ECC.generate(curve='P-256')
+    return 2 * a
 
 def scalar_mult(k, a):
     # Помножити точку a на скаляр k
-    # Реалізуйте відповідний код
-    return c
+    curve = ECC.generate(curve='P-256')
+    return k * a
 
 def ec_point_to_string(point):
     # Перетворити точку в рядок
-    # Реалізуйте відповідний код
-    return s
+    return f"{point.x},{point.y}"
 
 def string_to_ec_point(s):
     # Перетворити рядок в точку
-    # Реалізуйте відповідний код
-    return point
+    x, y = map(int, s.split(","))
+    curve = ECC.generate(curve='P-256')
+    return curve.point(x, y)
 
 def print_ec_point(point):
     # Вивести координати точки на ЕК
-    # Реалізуйте відповідний код
     print(f"({point.x}, {point.y})")
 
 
 G = base_point_get()
-k = SetRandom(256)
-d = SetRandom(256)
+k = getRandomInteger(256)
+d = getRandomInteger(256)
 
 H1 = scalar_mult(d, G)
 H2 = scalar_mult(k, H1)
