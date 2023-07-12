257197f1f5acb8f65781ccb7d97024de9c11e664
Сіплий <maksdrap228@gmail.com>
2023-07-12 14:44:38 +0300

    Код використовує переданий об'єкт curve для всіх операцій на еліптичних точках та перевірці належності до кривої

--- 1lab.py
+++ 1lab.py
@@ -1,35 +1,55 @@
 from Crypto.Util.number import getRandomInteger
 from Crypto.PublicKey import ECC
 
-
-def base_point_get():
-    # Отримайте базову точку G
-    curve = ECC.generate(curve='P-256')
+def base_point_get(curve):
+    # Отримати базову точку G
     return curve.pointQ
 
+def ec_point_gen(curve, x, y):
+    # Створити точку ЕК з координатами (x, y)
+    return curve.point(x, y)
 
-def scalar_mult(k, a):
-    # Помножте точку a на скаляр k
-    curve = ECC.generate(curve='P-256')
+def is_on_curve_check(curve, point):
+    # Перевірити, чи належить точка point до кривої
+    return curve.contains(point)
+
+def add_ec_points(curve, a, b):
+    # Додати точки a і b на ЕК
+    return a + b
+
+def double_ec_point(curve, a):
+    # Подвоїти точку a на ЕК
+    return 2 * a
+
+def scalar_mult(curve, k, a):
+    # Помножити точку a на скаляр k
     return k * a
 
+def ec_point_to_string(point):
+    # Перетворити точку в рядок
+    return f"{point.x},{point.y}"
 
-def check_equation():
-    G = base_point_get()
-    k = getRandomInteger(256)
-    d = getRandomInteger(256)
+def string_to_ec_point(curve, s):
+    # Перетворити рядок в точку
+    x, y = map(int, s.split(","))
+    return curve.point(x, y)
 
-    H1 = scalar_mult(d, G)
-    H2 = scalar_mult(k, H1)
-
-    H3 = scalar_mult(k, G)
-    H4 = scalar_mult(d, H3)
-
-    result = H2 == H4
-
-    return result
+def print_ec_point(point):
+    # Вивести координати точки на ЕК
+    print(f"({point.x}, {point.y})")
 
 
-# Виконайте перевірку рівняння
-equation_result = check_equation()
-print("Equation Result:", equation_result)
+curve = ECC.generate(curve='P-256')
+G = base_point_get(curve)
+k = getRandomInteger(256)
+d = getRandomInteger(256)
+
+H1 = scalar_mult(curve, d, G)
+H2 = scalar_mult(curve, k, H1)
+
+H3 = scalar_mult(curve, k, G)
+H4 = scalar_mult(curve, d, H3)
+
+result = H2 == H4
+
+print(result)
